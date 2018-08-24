0e2cb45f6c52b8125e09d4aa8f0bec9f597fd7f2
matiur rahman <matiur@matiurs-MacBook-Air.local>
2018-08-24 14:40:01 -0400

    refactor method

--- src/test/java/wordlength/UnitTest/TestWordLength.java
+++ src/test/java/wordlength/UnitTest/TestWordLength.java
@@ -12,8 +12,8 @@ public class TestWordLength {
     @Test
     public void largestWord(){
         Map<Integer, String> actualMap = new HashMap<Integer, String>();
-        actualMap.put(8, "happiest");
-        Map<Integer, String> expectedMap = DetermineWordLength.findTheLargestWord("Today is the happiest day of my life");
+        actualMap.put(6, "jumped");
+        Map<Integer, String> expectedMap = DetermineWordLength.findTheLargestWord("The cow jumped over the moon.");
         Assert.assertEquals(actualMap, expectedMap );
     }
 }
